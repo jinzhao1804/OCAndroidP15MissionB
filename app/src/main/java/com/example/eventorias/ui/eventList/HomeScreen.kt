4ac9a49417a788a76ae2bcf53d4e822b467b7f5a
jin zhao <jinzhaocnfr@gmail.com>
2024-12-27 16:01:03 +0100

    removed the vertical bar in the middle of the bars with containerColor

--- app/src/main/java/com/example/eventorias/ui/eventList/HomeScreen.kt
+++ app/src/main/java/com/example/eventorias/ui/eventList/HomeScreen.kt
@@ -1,6 +1,7 @@
 package com.example.eventorias.ui.eventList
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.List
@@ -13,12 +14,14 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.eventorias.ui.theme.dark
 import com.google.firebase.auth.FirebaseUser
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
@@ -34,7 +37,20 @@ fun HomeScreen(user: FirebaseUser, onSignOut: () -> Unit) {
             )
         },
         bottomBar = {
-            BottomNavigationBar(navController)
+            Box (modifier = Modifier.fillMaxWidth()
+                .background(dark)
+                .wrapContentSize(Alignment.Center),
+                contentAlignment = Alignment.Center
+            ){
+                Box(
+                    modifier = Modifier
+                        .width(200.dp)
+                    ,
+                    contentAlignment = Alignment.Center
+                ) {
+                    BottomNavigationBar(navController) // Put the nav bar inside the Box to center it
+                }
+            }
         },
         floatingActionButton = {
             FloatingActionButton(
@@ -60,13 +76,15 @@ fun HomeScreen(user: FirebaseUser, onSignOut: () -> Unit) {
 fun BottomNavigationBar(navController: NavController) {
     var selectedTabIndex by remember { mutableStateOf(0) }
 
-    NavigationBar {
+    NavigationBar(
+        modifier = Modifier.background(color = dark),
+        containerColor = dark
+    ) {
         NavigationBarItem(
             selected = selectedTabIndex == 0,
             onClick = {
                 selectedTabIndex = 0
                 navController.navigate("events") {
-                    // Avoid multiple backstack entries for the same screen
                     launchSingleTop = true
                     restoreState = true
                 }
@@ -74,14 +92,21 @@ fun BottomNavigationBar(navController: NavController) {
             icon = {
                 Icon(Icons.Filled.List, contentDescription = "Events")
             },
-            label = { Text("Events") }
+            label = { Text("Events") },
+            modifier = Modifier.background(dark).padding(horizontal = 0.dp), // Change background color when selected
+            colors = NavigationBarItemDefaults.colors(
+                unselectedIconColor = Color.White,  // White color for unselected icons
+                selectedIconColor = Color.White,    // White color for selected icons
+                unselectedTextColor = Color.White,  // White color for unselected text
+                selectedTextColor = Color.White     // White color for selected text
+            )
         )
+
         NavigationBarItem(
             selected = selectedTabIndex == 1,
             onClick = {
                 selectedTabIndex = 1
                 navController.navigate("profile") {
-                    // Avoid multiple backstack entries for the same screen
                     launchSingleTop = true
                     restoreState = true
                 }
@@ -89,16 +114,25 @@ fun BottomNavigationBar(navController: NavController) {
             icon = {
                 Icon(Icons.Filled.Person, contentDescription = "Profile")
             },
-            label = { Text("Profile") }
+            label = { Text("Profile") },
+            modifier = Modifier.background(dark).padding(horizontal = 0.dp),
+            colors = NavigationBarItemDefaults.colors(
+                unselectedIconColor = Color.White,  // White color for unselected icons
+                selectedIconColor = Color.White,    // White color for selected icons
+                unselectedTextColor = Color.White,  // White color for unselected text
+                selectedTextColor = Color.White     // White color for selected text
+            )
         )
     }
 }
 
+
+
 @Composable
 fun EventListScreen(user: FirebaseUser, onSignOut: () -> Unit) {
     Column(modifier = Modifier.padding(16.dp)) {
+        Spacer(modifier = Modifier.height(100.dp))
         Text(text = "Hi ${user.displayName ?: "User"}")
-        Spacer(modifier = Modifier.height(8.dp)) // Adding space between Text and Button
         Button(onClick = onSignOut) {
             Text(text = "Sign Out")
         }
@@ -109,6 +143,7 @@ fun EventListScreen(user: FirebaseUser, onSignOut: () -> Unit) {
 @Composable
 fun ProfileScreen(user: FirebaseUser) {
     Column(modifier = Modifier.padding(16.dp)) {
+        Spacer(modifier = Modifier.height(100.dp)) // Adding space between Text and Button
         Text(text = "Profile of ${user.displayName ?: "User"}")
         // Add Profile content here
     }
