4a3fe9fb99e2002d18f5eb135ebf2ebee2fe644f
jin zhao <jinzhaocnfr@gmail.com>
2025-01-12 10:11:02 +0100

    gradle change for sonar

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,12 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
     id("com.google.gms.google-services") version "4.4.2" apply false
+    id("org.sonarqube") version "6.0.1.5171"
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "myProjectKey")
+        property("sonar.host.url", "myHostUrl")
+    }
 }
